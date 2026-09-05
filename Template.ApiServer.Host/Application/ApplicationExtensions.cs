f8f0c26fde0553943e1a2870d27a666a7b71f08f
usausa <machi.pon@gmail.com>
2026-09-05 18:27:39 +0900

    Update

--- Template.ApiServer.Host/Application/ApplicationExtensions.cs
+++ Template.ApiServer.Host/Application/ApplicationExtensions.cs
@@ -620,7 +620,7 @@ public static class ApplicationExtensions
         {
             0 => null,
             1 => listeners[0],
-            _ => new ChainListener(listeners.ToArray())
+            _ => new ChainListener(listeners)
         };
     }
 
